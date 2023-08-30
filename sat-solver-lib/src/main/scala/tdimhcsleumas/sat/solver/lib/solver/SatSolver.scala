70a87d094ebaed5fd12ecd05c03dd89b750644cd
Samuel Schmidt <tdimhcsleumas@gmail.com>
2023-08-30 00:29:31 -0700

    added mapper class

--- sat-solver-lib/src/main/scala/tdimhcsleumas/sat/solver/lib/solver/SatSolver.scala
+++ sat-solver-lib/src/main/scala/tdimhcsleumas/sat/solver/lib/solver/SatSolver.scala
@@ -2,6 +2,7 @@ package tdimhcsleumas.sat.solver.lib.solver
 
 import tdimhcsleumas.sat.solver.lib.algorithms._
 import tdimhcsleumas.sat.solver.lib.domain._
+import tdimhcsleumas.sat.solver.lib.mapper._
 import org.log4s._
 
 class SatSolver(private val alg: AlgTrait) {
@@ -10,25 +11,12 @@ class SatSolver(private val alg: AlgTrait) {
     def solve[A](cnf: CNF[A]): Option[Assignment[A]] = {
         logger.info(f"Solving cnf with ${cnf.clauses.length} clauses with ${alg.getClass}")
 
-        val variables = cnf.clauses.flatMap(_.literals.map(_.variable))
-            .toSet
-            .toList
-
-        val variableIntMapping = variables.zipWithIndex.foldLeft(Map[A, Int]()) { (map, pair) =>
-            val (variable, index) = pair
-            map + ((variable, index + 1))
-        }
-
-        // the first index shows up twice to account for off-by-one mapping
-        val variableArray = variables.headOption match {
-            case Some(head) => Vector[A](head) ++ variables.toVector
-            case None => Vector[A]()
-        }
+        val mapper = new LiteralMapper(cnf)
 
         val intCnf = cnf.clauses.map { clause =>
             clause.literals.map { literal =>
                 val Literal(variable, defined) = literal
-                val int = variableIntMapping.getOrElse(variable, throw new Exception("could not find variable"))
+                val int = mapper.getInt(variable).get
                 if (defined) {
                     int
                 } else {
@@ -41,7 +29,7 @@ class SatSolver(private val alg: AlgTrait) {
             intSolution <- alg.solve(intCnf)
             unwrapped <- {
                 val literals = intSolution.map { asgn => 
-                    val variable = variableArray(asgn.abs)
+                    val variable = mapper.getVariable(asgn.abs).get
                     Literal(variable, asgn > 0)
                 }.toList
 
---
+++ sat-solver-lib/src/test/scala/tdimhcsleumas/sat/solver/lib/mapper/LiteralMapperSpec.scala
@@ -0,0 +1,31 @@
+package tdimhcsleumas.sat.solver.lib.mapper
+
+import tdimhcsleumas.sat.solver.lib.domain._
+
+import org.scalatest._
+import funspec._
+
+class LiteralMapperSpec extends AnyFunSpec {
+    it("maps data correctly") {
+        val firstVar = (1, (1, 1))
+        val secondVar = (2, (1, 2))
+        val thirdVar = (3, (1, 2))
+
+        val cnf = CNF.builder()
+            .addClause(Clause(Literal(firstVar, false), Literal(firstVar, true), Literal(secondVar, false)))
+            .addClause(Clause(Literal(firstVar, true), Literal(thirdVar, false)))
+            .addClause(Clause(Literal(thirdVar, true), Literal(secondVar, false)))
+            .addClause(Clause(Literal(secondVar,true)))
+            .build()
+        
+        val mapper = new LiteralMapper(cnf)
+
+        assert(mapper.getInt(firstVar).get === 1)
+        assert(mapper.getInt(secondVar).get === 2)
+        assert(mapper.getInt(thirdVar).get === 3)
+
+        assert(firstVar === mapper.getVariable(1).get)
+        assert(secondVar === mapper.getVariable(2).get)
+        assert(thirdVar === mapper.getVariable(3).get)
+    }
+}
