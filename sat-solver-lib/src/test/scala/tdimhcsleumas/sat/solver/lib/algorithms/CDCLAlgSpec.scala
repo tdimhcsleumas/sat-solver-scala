db938dd5709b1437b6f015fe343b9096525d1715
Samuel Schmidt <tdimhcsleumas@gmail.com>
2023-09-01 23:52:08 -0700

    add CDCLAlgSpec

---
+++ sat-solver-lib/src/test/scala/tdimhcsleumas/sat/solver/lib/algorithms/CDCLAlgSpec.scala
@@ -0,0 +1,48 @@
+package tdimhcsleumas.sat.solver.lib.algorithms
+
+import org.scalatest._
+import funspec._
+
+class CDCLAlg extends AnyFunSpec {
+    it("returns an assignment for a solvable problem") {
+        val problem = Seq(
+            Seq(1, 2, 3, 5, -6),
+            Seq(7, -6, -5, 2),
+            Seq(8, 9, 7),
+            Seq(4, -2, 1),
+            Seq(9, 5, -6),
+        )
+
+        val solver = new CDCLAlg
+
+        val maybeAssignment = solver.solve(problem)
+
+        assert(maybeAssignment.isDefined)
+
+        val assignment = maybeAssignment.get
+
+        problem.foreach { clause =>
+            val filtered = clause.filter(literal => assignment.contains(literal))
+            assert(!filtered.isEmpty)
+        }
+    }
+
+    it("retuns None when a problem is unsolvable") {
+        val problem = Seq(
+            Seq(1, 2, 3),
+            Seq(1, 2, -3),
+            Seq(1, -2, 3),
+            Seq(-1, 2, 3),
+            Seq(1, -2, -3),
+            Seq(-1, -2, 3),
+            Seq(-1, 2, -3),
+            Seq(-1, -2, -3),
+        )
+
+        val solver = new CDCLAlg
+
+        val maybeAssignment = solver.solve(problem)
+
+        assert(maybeAssignment.isEmpty)
+    }
+}
